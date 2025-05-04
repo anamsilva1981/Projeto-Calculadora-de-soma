02f17bfdad9c2eac5f1f9cdb4d239a0aa134b100
anamsilva1981 <ana.msilva@hotmail.com>
2025-05-04 06:28:39 -0300

    [Feature] Adiciona suporte ao FormsModule e melhora a lógica de soma na calculadora

--- src/app/calculadora/calculadora.component.ts
+++ src/app/calculadora/calculadora.component.ts
@@ -1,19 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-calculadora',
-  imports: [],
+  imports: [FormsModule],
+  standalone: true,
   templateUrl: './calculadora.component.html',
   styleUrl: './calculadora.component.css'
 })
 export class CalculadoraComponent {
 
   public resultado: number = 0;
-  public numero1: number = 0;
-  public numero2: number = 0;
+  public numero1?: number;
+  public numero2?: number;
 
   public somaValores(){
     console.log('click no botão de soma');
-    this.resultado = this.numero1 + this.numero2;
+    this.resultado = (this.numero1 ?? 0) + (this.numero2 ?? 0);
   }
 }
